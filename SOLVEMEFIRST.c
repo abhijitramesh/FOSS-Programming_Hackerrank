9900f9d5c47f989af4af05aa83bdae76dbb2b046
abhijitramesh <43090559+abhijitramesh@users.noreply.github.com>
2018-10-17 20:09:48 +0530

    Commented
    added comments

--- SOLVEMEFIRST.c
+++ SOLVEMEFIRST.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio.h>//importing c packages
 
 int solveMeFirst(int x, int y);
-int main(void)
+int main(void)//main program
 {
-  printf("enter two numbers:");
-  int x=get_int();
-  int y=get_int();
-  int sum=solveMeFirst(x,y);
-  printf("sumof the two numbers is %i", sum);
+  printf("enter two numbers:");//inputiing the numbers
+  int x=get_int();//storing to variable x
+  int y=get_int();//storing to variable y
+  int sum=solveMeFirst(x,y);//calling function to add the inputted numbers
+  printf("sumof the two numbers is %i", sum);//outputting the final result
 }
-int solveMeFirst(int x, int y)
+int solveMeFirst(int x, int y)//sunction to add the numbers
 {
-  int sum=x+y;
-  return sum;
+  int sum=x+y;//adding the mnumbers
+  return sum;//returning the values as integer
 }
